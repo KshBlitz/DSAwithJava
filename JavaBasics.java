ee7979afbfbd1cec438b46059194e0cf8aac30a6
KshBlitz <mahajankalash9@gmail.com>
2023-12-18 21:32:01 +0530

    Learning DSA with Java

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class JavaBasics{
+    public static void main(String[] args){
+        byte b = 25;
+        byte a = (byte) (b * 2);
+        System.out.println(a);
+    }
+}   
