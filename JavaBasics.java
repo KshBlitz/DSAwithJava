2760ae07695b29866cd4de4e9a17395a7a6630b4
KshBlitz <mahajankalash9@gmail.com>
2023-12-18 21:39:09 +0530

    Deleting useless code

--- JavaBasics.java
+++ JavaBasics.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class JavaBasics{
     public static void main(String[] args){
-        byte b = 25;
+        byte b = 23;
         byte a = (byte) (b * 2);
         System.out.println(a);
     }
