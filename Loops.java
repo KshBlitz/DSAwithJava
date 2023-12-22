de76086b2aa4e58e4633a432e50132416941ef91
Kalash Mahajan <mahajankalash9@gmail.com>
2023-12-22 13:37:17 +0530

    Questions on Conditonal ststements

---
+++ Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String[] args) {
+        Counter();
+    }
+
+    static void Counter(){
+        int counter = 0;
+        while(counter < 3){
+            System.out.println("Wassup");
+            counter++;
+        }
+    }
+}
