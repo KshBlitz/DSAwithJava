d4e9b2c0f36020361407dd68e5b63e1dbee4dce6
Kalash Mahajan <mahajankalash9@gmail.com>
2023-12-24 20:10:23 +0530

    Soled Loops Questions

--- LoopsQuestion.java
+++ LoopsQuestion.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class LoopsQuestion {
     public static void main(String[] args) {
         //UserBreak();
-        PrimeNumber();
+        //PrimeNumber();
+        //Factorial();
+        //SumQues();
+        MultiplicationTable();
     }
 
     static void UserBreak(){
@@ -43,5 +46,58 @@ public class LoopsQuestion {
             System.out.println("Number is not prime");
         }
         }
-    }    
-}
+    }  
+    
+    static void SumQues(){
+        Scanner sc = new Scanner(System.in);
+        
+        int number;
+        int choice;
+        int evenSum=0;
+        int oddSum=0;
+
+        do{
+            System.out.println("Enter your number: ");
+            number = sc.nextInt();
+            if(number%2==0){
+                evenSum = evenSum + number;
+            }
+            else{
+                oddSum = oddSum + number;
+            }
+            System.out.println("Enter 1 to continue or 0 to quit: ");
+            choice = sc.nextInt();
+        } 
+        while(choice == 1);
+
+        System.out.println("Even sum: " + evenSum);
+        System.out.println("Odd sum: " + oddSum);
+        
+    }
+
+    static void Factorial(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+        int fact = 1;
+        
+        for(int i = 1; i<=num; i++){
+            fact = fact*i;
+        }  
+        System.out.println(fact); 
+    }
+
+    static void MultiplicationTable(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+        int X = num ;
+
+        System.out.println("Here is the multiplication table of " + num);
+
+        for(int i = 0; i<=9; i++){
+            System.out.println(X);
+            X += num ;
+        }
+    }
+}    
