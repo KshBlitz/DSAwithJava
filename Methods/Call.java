516532a0f60a212c7baf882db64e5aae0838de04
Kalash Mahajan <mahajankalash8@gmail.com>
2025-02-11 19:58:32 +0530

    test

--- Methods/Call.java
+++ Methods/Call.java
@@ -1,15 +1,3 @@
-public class Call {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        swap(a,b);
-    }
-
-    public static void swap(int a, int b){
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println(a);
-        System.out.println(b);
-    }
+public static void main(String args[]){
+    
 }
--- Methods/Method.java
+++ Methods/Method.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Method {
+public class Method{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two number: ");
